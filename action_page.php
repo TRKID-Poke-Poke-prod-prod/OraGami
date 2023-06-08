017a903eb662eccdd260248d765ac220ffb7c6aa
TRKID-Poke-Poke-prod-prod <102126010+TRKID-Poke-Poke-prod-prod@users.noreply.github.com>
2023-06-08 16:06:29 +1000

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,6 +1,8 @@
 <?php
     $usname = $_POST["uname"];
     $password = $_POST["psw"];
+    echo($passowrd);
+    echo("Really?");
 
     $con = new mysqli("localhost",'root',"test","OraGami")
     if($con->connect_error) {
